c2b48663d8429ffba276fb060f4c1951c7cb2114
Michael O'Brien <obrienmn@tpg.com.au>
2014-02-24 17:06:37 +1100

    Added a new function to the web app's JS to report back an element's DOM position and created a new script bundle.

--- Seating Planner Web/App_Start/BundleConfig.cs
+++ Seating Planner Web/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace Seating_Planner_Web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/seatingplanner").Include(
+                "~/Scripts/seatingplanner.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
---
+++ Seating Planner Web/Scripts/seatingplanner.js
@@ -0,0 +1,30 @@
+﻿function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("Text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("Text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+
+// Reports the position of an element in the DOM
+// Based upon the function defined here http://www.kirupa.com/html5/get_element_position_using_javascript.htm
+function getPosition(element) {
+    var x = 0;
+    var y = 0;
+
+    while (element) {
+        x += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+        y += (element.offsetTop - element.scrollTop + element.clientTop);
+
+        element = element.offsetParent;
+    }
+
+    return [ x, y ];
+}
